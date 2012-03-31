c834feaf1a9f35cccf6aa816caf0e0834bf4e7e3
Aaron Decker <d00ble.ard@gmail.com>
2012-03-31 19:47:50 -0400

    added date restriction to search

--- php/load_events_by_location.php
+++ php/load_events_by_location.php
@@ -2,8 +2,11 @@
 require('cxn.php');
 
 // RESTRICTION ON ONLY NEW EVENTS PULLED
-$date_search = date("Y-m-d H:m:s", strtotime(time() - 60*60*12)); // 12 HOURS EARLIER
+$date_search = date("Y-m-d H:m:s", time() - 60*60*24*5); // 12 HOURS EARLIER
+$date_search_2 = date("Y-m-d H:m:s", time() + 60*60*24*14); // two weeks ahead
+//echo "from: ".$date_search." to: ".$date_search_2;
 define("DATE_TO_SEARCH_FROM", $date_search);
+define("DATE_TO_SEARCH_TO", $date_search_2);
 
 // ONLY FOR YQL EVENTS
 function search_output_func_YQL($all_vars){
@@ -11,14 +14,14 @@ function search_output_func_YQL($all_vars){
 	
 	$day = format_date($start_date);
 	$hour = format_time($start_date);
-	$addy = get_address($event_id);
+	$addy = get_address_YQL($event_id);
 
 	$search_output .= "
 	<div class='eventSingle'>
 		<table>
 		<tr>
 		<td>
-			<h3>".strip_tags($event_title)."</h3><br>
+			<h3>".strip_tags($event_title)."</h3>
 			Date: $day  Time: $hour <br>
 			Country: $country_name <br>
 			Location: $addy, $venue_state, $venue_zip <br>
@@ -30,11 +33,41 @@ function search_output_func_YQL($all_vars){
 		</tr>
 		</table>
 	</div>
-	<p>&nbsp;</p>
+	<br />
 	";
 
 	return $search_output;
 	}
+	
+function search_output_func_users($all_vars){
+	extract($all_vars);
+	
+	$day = format_date($start_date);
+	$hour = format_time($start_date);
+	//$addy = get_address($event_id);
+
+	$search_output .= "
+	<div class='eventSingle'>
+		<table>
+		<tr>
+		<td>
+			<h3>".strip_tags($event_title)."</h3>
+			Date: $day  Time: $hour <br>
+			Location: $venue_address<br>
+			Description: ".strip_tags($event_description)."<br>	
+			Distance: ".round($distance, 1)." miles <br>
+			Id: $event_id <br>
+		</td>	
+		</tr>
+		</table>
+	</div>
+	<br />
+	";
+	
+	
+	return $search_output;
+	}
+
 
 // DISTANCE CALC
 function distance($lat1, $lon1, $lat2, $lon2, $unit) { 
@@ -91,14 +124,20 @@ function sortByOneKey(array $array, $key, $asc = true) {
 	
 	
 // YQL GET ADDY
-function get_address($event_id){
+function get_address_YQL($event_id){
 	$cxn = $GLOBALS['cxn'];
-	$qry = "SELECT * FROM YQL_event_address WHERE event_id='$event_id'";
+	$qry = "SELECT * 
+			FROM YQL_event_address
+			WHERE 
+			event_id = '$event_id'
+			";
+			
 	$res = mysqli_query($cxn, $qry)
 		or die ("failed to get address".mysqli_error($cxn));
 	$row = mysqli_fetch_assoc($res);
 	return $row['address_text'];
 	}
+	
 // YQL GET LAN LNG
 function get_event_lat_lng($event_id){
 	$cxn = $GLOBALS['cxn'];
@@ -115,8 +154,7 @@ function get_event_lat_lng($event_id){
 	}
 
 // pulls event info based on ID
-function get_all_event_list_YQL($event_id)
-	{
+function get_all_event_list_YQL($event_id){
 	$cxn = $GLOBALS['cxn'];
 	$qry = "SELECT * FROM YQL_events WHERE '$event_id' = event_id";
 	$res = mysqli_query($cxn,$qry)
@@ -125,18 +163,32 @@ function get_all_event_list_YQL($event_id)
     return $row2;
 	}
 
-
+function get_all_event_list_users($event_id){
+	$cxn = $GLOBALS['cxn'];
+	$qry = "SELECT * FROM user_events WHERE '$event_id' = event_id";
+	$res = mysqli_query($cxn,$qry)
+		or   die ("Couldn't execute query.".mysqli_error($cxn));
+    $row2 = mysqli_fetch_assoc($res);
+    return $row2;
+	}
 
 
 // YQL GET ADDRESS
-function pull_YQL_events($lat, $lon){
+function pull_ALL_events($lat, $lon){
+	
+	// DO YQL SECTION FIRST
 	$cxn = $GLOBALS['cxn'];
-	$qry = "SELECT * FROM YQL_event_address";
+	$d = DATE_TO_SEARCH_FROM;
+	$d2 = DATE_TO_SEARCH_TO;
+	$qry = "SELECT * FROM YQL_event_address
+			WHERE 
+			'$d' <= (SELECT end_date FROM YQL_events 
+			WHERE event_id = YQL_event_address.event_id)
+			";
 	$res = mysqli_query($cxn, $qry)
 		or die ("couldn't do the db loc thing...");
 	
 	$main_array = array(); // create an emptry array
-	
 	while($row = mysqli_fetch_assoc($res)){
 		extract($row);
 		$d_total = distance($lat, $lon, $x_coord, $y_coord, "m");
@@ -146,14 +198,42 @@ function pull_YQL_events($lat, $lon){
 			"origin" => "YQL", 
 			"id" => $event_id,
 			"distance" => $d_total,
-			//"index" => 0,
-			"lat" => $lat,
-			"lon" => $lon
+			"address_DB" => 0,
+			"lat" => $x_coord,
+			"lon" => $y_coord
 			);
-	
 		//$distances[$event_id] = $d_total;
 		array_push($main_array, $event);
 		}//end compare and extract loc data while.
+	
+	
+	// DO USER SECTION NEXT
+	$qry = "SELECT * FROM event_address
+			WHERE
+			'$d' <= (SELECT end_date FROM user_events 
+			WHERE event_id = event_address.event_id)
+			";
+	$res = mysqli_query($cxn, $qry)
+		or die ("couldn't do the db loc thing...");
+	
+	while($row = mysqli_fetch_assoc($res)){
+		extract($row);
+		$d_total = distance($lat, $lon, $x_coord, $y_coord, "m");
+		// the sub array
+		$event = array(
+			"origin" => "user", 
+			"id" => $event_id,
+			"distance" => $d_total,
+			"address_DB" => $address_text,
+			"lat" => $x_coord,
+			"lon" => $y_coord
+			);
+		//$distances[$event_id] = $d_total;
+		array_push($main_array, $event);
+		}//end compare and extract loc data while.
+	
+	
+	
 	
 	// NOW SORT THIS multidimen array
 	$main_array = sortByOneKey($main_array, "distance");
@@ -161,16 +241,12 @@ function pull_YQL_events($lat, $lon){
 	// MAIN LOOP
 	$search_output = "";
 	foreach ($main_array as $event_array){
-		//$event_array = $main_array[$i];
-		
-/*
-		echo "<pre>";
-		print_r($event_array);
-		echo "</pre>";
-		
-*/
+			/*
+					echo "<pre>";
+					print_r($event_array);
+					echo "</pre>";
+			*/
 		// THIS PULLS EVENT INFO VIA ID
-		//echo "id: $event_id";
 		//print_r($event_array);
 		extract($event_array);
 		
@@ -198,7 +274,24 @@ function pull_YQL_events($lat, $lon){
 			}
 		else if($origin == "user"){
 			// do user stuff here
+			$event_row = get_all_event_list_users($id);
+			if($event_row != null){
+				extract($event_row);
+				}
 			
+			$all_vars = array(
+				"event_id" => $event_id,
+				"event_description" => $event_description,
+				"event_title" => $event_title,
+				"start_date" => $start_date,
+				"end_date" => $end_date,
+				"venue_address" => $address_DB,
+				"distance"=> $distance,
+				"search_output" => $search_output
+				);
+		
+			$search_output = search_output_func_users($all_vars); //see search_functions.php
+				
 			}
 		}//end loop for location search	 FOREACH
 	return $search_output;
@@ -209,7 +302,7 @@ function pull_YQL_events($lat, $lon){
 $lat = $_REQUEST['latitude'];
 $lon = $_REQUEST['longitude'];
 
-$search_output = pull_YQL_events($lat, $lon);
+$search_output = pull_ALL_events($lat, $lon);
 
 // spit out the formatted stuff
 echo $search_output;	
